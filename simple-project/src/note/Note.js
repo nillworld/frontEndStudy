e1b33f598411789eb2ee18590131a4c6bc62b9e5
nill <wwaosldk@gmail.com>
2022-01-25 20:39:24 +0900

    onenote amd memo

--- simple-project/src/note/Note.js
+++ simple-project/src/note/Note.js
@@ -3,5 +3,11 @@
 // window는 OneNote API를 통한 연동
 // https://docs.microsoft.com/en-us/graph/integrate-with-onenote
 
-// noteStation을 위해 Joplin?..
+// noteStation을 위해 Joplin?..//
 // https://reinia.net/2582
+
+// 스마트폰 윈도우 메모 연동.
+// 원노트 - 윈도우 메모
+// https://addplus.tistory.com/entry/%EC%9C%88%EB%8F%84%EC%9A%B010-Sticky-Notes%EC%8A%A4%ED%8B%B0%EC%BB%A4-%EB%A9%94%EB%AA%A8-%EC%8A%A4%EB%A7%88%ED%8A%B8%ED%8F%B0%EA%B3%BC-%EC%97%B0%EB%8F%99%EB%8F%99%EA%B8%B0%ED%99%94%ED%95%98%EA%B8%B0
+
+// 웹 - 원노트 aip - 원노트 - 스마트폰
