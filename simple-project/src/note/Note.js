326b2cb7efc7817a8fe9ac7665c7d01a56aa9ec0
nill <wwaosldk@gmail.com>
2022-01-27 21:29:22 +0900

    api note

--- simple-project/src/note/Note.js
+++ simple-project/src/note/Note.js
@@ -11,3 +11,4 @@
 // https://addplus.tistory.com/entry/%EC%9C%88%EB%8F%84%EC%9A%B010-Sticky-Notes%EC%8A%A4%ED%8B%B0%EC%BB%A4-%EB%A9%94%EB%AA%A8-%EC%8A%A4%EB%A7%88%ED%8A%B8%ED%8F%B0%EA%B3%BC-%EC%97%B0%EB%8F%99%EB%8F%99%EA%B8%B0%ED%99%94%ED%95%98%EA%B8%B0
 
 // 웹 - 원노트 aip - 원노트 - 스마트폰
+// ios cloud api
