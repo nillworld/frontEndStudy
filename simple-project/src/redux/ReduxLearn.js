5562850258952dc2de6dcc73c0d2f7ee0f64f689
nill <wwaosldk@gmail.com>
2022-02-01 22:39:12 +0900

    redux studing

--- simple-project/src/redux/ReduxLearn.js
+++ simple-project/src/redux/ReduxLearn.js
@@ -1,19 +1,29 @@
 import { createStore } from "redux";
+import { Provider, useSelector, useDispatch, connect } from "react-redux";
 
-function reducer(currentStore, action) {
-  if (currentStore === undefined) {
+//state 안에 있는 store를 어떻게 바꿀것인가
+function reducer(currentState, action) {
+  if (currentState === undefined) {
     return { number: 1 };
   }
-  const newState = { ...currentStore };
+  const newState = { ...currentState };
   return newState;
 }
 
 const store = createStore(reducer);
 
+const test1 = () => {
+  return <div>hi</div>;
+};
+
 const ReduxLearn = () => {
   return (
     <div>
-      <div>rr</div>
+      {/* provider는 store를 제공할 범위 설정/ 반드시 store 필요 */}
+      <Provider store={store}>
+        <div>rr</div>
+        <test1></test1>
+      </Provider>
     </div>
   );
 };
