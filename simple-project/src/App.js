80b99cb1a886b35f60413231369f668603254902
nill <wwaosldk@gmail.com>
2022-02-02 23:51:51 +0900

    redux studing

--- simple-project/src/App.js
+++ simple-project/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import GachaGame from "./gachaGame/GachaGame";
+import ReduxLearn from "./redux/reduxLearn";
 import { Reducer } from "react";
 
 function App() {
-  return <GachaGame />;
+  return <ReduxLearn />;
 }
 
 export default App;
