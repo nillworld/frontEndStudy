d767a949ee98c7b99b138194d4666ec7de2087de
nill <wwaosldk@gmail.com>
2022-02-02 23:54:07 +0900

    react- redux

--- simple-project/src/App.js
+++ simple-project/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import GachaGame from "./gachaGame/GachaGame";
-import ReduxLearn from "./redux/reduxLearn";
+import ReduxLearn from "./redux/ReduxLearn";
 import { Reducer } from "react";
 
 function App() {
